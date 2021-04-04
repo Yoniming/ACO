19b2855e70e4b6e14a506cdd7b42ee6120f9f5af
baoyixing <baoyixing@localhost.localdomain>
2021-04-04 20:34:56 +0800

    add basic code

---
+++ src/Timer.h
@@ -0,0 +1,33 @@
+//
+// Created by baoyixing on 3/29/21.
+//
+
+#ifndef CODE_TIMER_H
+#define CODE_TIMER_H
+
+#include <atomic>
+#include <chrono>
+#include <condition_variable>
+#include <functional>
+#include <memory>
+#include <thread>
+
+
+class Timer {
+public:
+	Timer();
+	Timer(Timer& timer);
+	~Timer();
+	void start(int interval, const std::function<void()>& task);
+	static void startOnce(int delay, const std::function<void()>& task);
+	void stop();
+
+private:
+	std::atomic<bool> _expired {true};			 // timer stopped status
+	std::atomic<bool> _try_to_expire {false};	 // timer is in stop process
+	std::mutex _mutex;
+	std::condition_variable _expired_cond;
+};
+
+
+#endif	  //CODE_TIMER_H
